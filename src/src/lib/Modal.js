3a98d489b8475e122c8fe8499639afdff5e8bb4d
Chorro Michael <michaelchorro.dev@gmail.com>
2022-12-06 10:41:28 +0100

    changement de couleurs des éléments

--- src/src/lib/Modal.js
+++ src/src/lib/Modal.js
@@ -5,6 +5,7 @@ function Modal() {
   return (
     <ModalContainer>
       <ModalTitle>Successfully created employee !</ModalTitle>
+      <Button>Close</Button>
     </ModalContainer>
   )
 }
@@ -15,17 +16,27 @@ const ModalContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
+  gap: 2rem;
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
   width: 60%;
   height: 300px;
-  background-color: blue;
+  background-color: #f1f8e9;
   margin: 0 auto;
   border-radius: 10px;
 `
 
 const ModalTitle = styled.h2`
-  font-size: 36px;
+  font-size: 2rem;
+`
+const Button = styled.button`
+  background-color: #558b2f;
+  color: white;
+  font-size: 1.25rem;
+  padding: 0.5em 1.5em;
+  border-radius: 5px;
+  border: none;
 `
